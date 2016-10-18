39eb9c03a30215fab245c44580ed015c79f08615
Meder <m.toktosunov@icloud.com>
2016-10-18 00:42:08 -0400

    second

--- lib/airplane.rb
+++ lib/airplane.rb
@@ -6,8 +6,37 @@ class Airplane
     @type = type
     @wing_load = wing_load
     @horsepower = horsepower
-    @ground = true
     @engine = false
+    @ground = true
+  end
+
+  def start
+    if @engine == false
+      @engine = true
+      return 'Airplane started'
+    else
+      return 'already started'
+    end
+
+  end
+
+  def land
+    if @engine == true && @ground == false
+      @engine = false
+      @ground = true
+      return 'Landed safely'
+    end
+  end
+
+  def takeoff
+    if @engine == false
+      @engine = true
+      @ground = false
+      puts "Up in the air"
+    else
+      @ground = false
+      return "Up in the air"
+    end
   end
 
 end
--- spec/lib/airplane_spec.rb
+++ spec/lib/airplane_spec.rb
@@ -1,19 +1,34 @@
 require_relative "../../lib/airplane"
 
 
-describe Airplane do
+RSpec.describe Airplane do
   my_airplane = Airplane.new("cesna", 10, 150)
 
   describe "#initialization" do
-    expect(my_airplane.type).to eq("cesna")
+    it "creates new Airplane" do
+      expect(my_airplane.type).to eq("cesna")
+      expect(my_airplane.wing_load).to eq(10)
+      expect(my_airplane.horsepower).to eq(150)
+    end
   end
 
   describe "#start" do
-  end
-
-  describe "#land" do
+    it "starts the engine" do
+      expect(my_airplane.start).to eq("Airplane started")
+      expect(my_airplane.start).not_to eq("Airplane is already started")
+    end
   end
 
   describe "#takeoff" do
+    it "lifts the plane to the air" do
+      expect(my_airplane.takeoff).to eq("Up in the air")
+    end
   end
+
+  describe "#land" do
+    it "lands the plane" do
+      expect(my_airplane.land).to eq('Landed safely')
+    end
+  end
+
 end
