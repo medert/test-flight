c52385cac04f16ffe82e37712a1f527c06f3d329
Meder <m.toktosunov@icloud.com>
2016-10-18 20:19:50 -0400

    Final commit

--- lib/airplane.rb
+++ lib/airplane.rb
@@ -1,42 +1,62 @@
 require 'pry'
 
 class Airplane
-  attr_accessor :type, :wing_load, :horsepower
-  def initialize (type, wing_load, horsepower)
+  attr_accessor :type, :wing_load, :horsepower, :fuel, :engine
+  def initialize (type, wing_load, horsepower, fuel)
     @type = type
     @wing_load = wing_load
     @horsepower = horsepower
     @engine = false
     @ground = true
+    @fuel = fuel
   end
 
   def start
-    if @engine == false
+    if @engine == false && @fuel >= 2
       @engine = true
+      @fuel -= 2
       return 'Airplane started'
+    elsif @engine == true
+      return 'Airplane is already started'
     else
-      return 'already started'
-    end
-
-  end
-
-  def land
-    if @engine == true && @ground == false
-      @engine = false
-      @ground = true
-      return 'Landed safely'
+      return 'Not enough fuel to start'
     end
   end
 
   def takeoff
-    if @engine == false
+    if @engine == true && @fuel >= 2 && @ground == true
       @engine = true
       @ground = false
-      puts "Up in the air"
+      @fuel -= 2
+      return "Airplane took off!"
+    elsif @engine == false && @ground == true
+      return "Engine is turned off. Start the engine!"
     else
-      @ground = false
-      return "Up in the air"
+      return "Not enough fuel to takeoff!"
     end
   end
 
+  def land
+    if @engine == true && @ground == false && @fuel >= 3
+      @engine = false
+      @ground = true
+      @fuel -= 3
+      return 'Landed safely!'
+    elsif @engine == true && @ground == false && @fuel < 3
+      return 'Not enough fuel to land!'
+    elsif @ground == true && @engine == true
+      return 'The plane is on the ground!'
+    elsif @engine == false && @ground == true
+      return 'The engine is turned off! The plane is on the ground!'
+    else
+      return 'Check for error!'
+    end
+  end
 end
+
+# code execution
+
+my_plane = Airplane.new('cesna', 10, 150, 10)
+puts my_plane.start
+puts my_plane.takeoff
+puts my_plane.land
--- spec/lib/airplane_spec.rb
+++ spec/lib/airplane_spec.rb
@@ -1,34 +1,78 @@
 require_relative "../../lib/airplane"
 
-
 RSpec.describe Airplane do
-  my_airplane = Airplane.new("cesna", 10, 150)
+  my_airplane = Airplane.new("cesna", 10, 150, 10)
 
   describe "#initialization" do
     it "creates new Airplane" do
       expect(my_airplane.type).to eq("cesna")
       expect(my_airplane.wing_load).to eq(10)
       expect(my_airplane.horsepower).to eq(150)
+      expect(my_airplane.fuel).to eq(10)
     end
   end
 
   describe "#start" do
     it "starts the engine" do
       expect(my_airplane.start).to eq("Airplane started")
-      expect(my_airplane.start).not_to eq("Airplane is already started")
+    end
+
+    it "is already strated" do
+      my_airplane.start
+      expect(my_airplane.start).to eq("Airplane is already started")
+    end
+
+    it "has enough fuel to start" do
+      my_airplane.start
+      expect(my_airplane.fuel).to eq(8)
+    end
+
+    it "does not have enough fuel to start" do
+      my_airplane = Airplane.new("cesna", 10, 150, 1)
+      expect(my_airplane.start).to eq("Not enough fuel to start")
     end
   end
 
   describe "#takeoff" do
     it "lifts the plane to the air" do
-      expect(my_airplane.takeoff).to eq("Up in the air")
+      my_airplane = Airplane.new("cesna", 10, 150, 8)
+      my_airplane.start
+      expect(my_airplane.takeoff).to eq("Airplane took off!")
+      expect(my_airplane.takeoff).to eq("Not enough fuel to takeoff!")
+    end
+
+    it "has INSIFICIENT fuel to takeoff" do
+      my_airplane = Airplane.new("cesna", 10, 150, 2)
+      my_airplane.start
+      expect(my_airplane.takeoff).to eq("Not enough fuel to takeoff!")
+      expect(my_airplane.takeoff).not_to eq("Airplane took off!")
     end
   end
 
   describe "#land" do
-    it "lands the plane" do
-      expect(my_airplane.land).to eq('Landed safely')
+    it "has enough fuel to land" do
+      my_airplane = Airplane.new("cesna", 10, 150, 7)
+      my_airplane.start
+      my_airplane.takeoff
+      expect(my_airplane.land).to eq('Landed safely!')
+    end
+
+    it "has INSIFICIENT fuel to land" do
+      my_airplane = Airplane.new("cesna", 10, 150, 6)
+      my_airplane.start
+      my_airplane.takeoff
+      expect(my_airplane.land).to eq('Not enough fuel to land!')
+    end
+
+    it "is on the ground" do
+      my_airplane = Airplane.new("cesna", 10, 150, 3)
+      my_airplane.start
+      expect(my_airplane.land).to eq('The plane is on the ground!')
+    end
+
+    it "is on the ground, the engine is turned off" do
+      my_airplane = Airplane.new("cesna", 10, 150, 2)
+      expect(my_airplane.land).to eq('The engine is turned off! The plane is on the ground!')
     end
   end
-
 end
